0d0708bc53a7b39781e2f8f6e08034902f195642
Frederik Böcker <fredboe@outlook.com>
2020-08-03 14:25:39 +0200

    added documentation
    added documentation

--- loadMNIST.py
+++ loadMNIST.py
@@ -5,22 +5,40 @@ import numpy as np
 
 
 def loadMNIST():
+    """
+        returns:
+                x_train, y_train, x_test, y_test -> mnist data from tensorflow
+    """
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    # updates the data to number between 0 and 1
     x_train = x_train/255
     x_test = x_test/255
+    # creates an 1-dimensional array out of the data
     x_train = [sample.reshape(1, 784)[0] for sample in x_train]
     x_test = [sample.reshape(1, 784)[0] for sample in x_test]
+    # creates an vector out of the label
     y_train = [add_one(y) for y in y_train]
     y_test = [add_one(y) for y in y_test]
     print("mnist loaded")
     return (x_train, y_train), (x_test, y_test)
 
+
 def add_one(index):
+    """
+        parameters:
+                    index: int between 0 and 9 (label)
+        returns:
+                array with a length of 10: 1 at the label else 0
+    """
     arr = np.zeros(10)
     arr[index] = 1.0
     return arr
 
+
 def plotMNIST_random():
+    """
+        plots a random image of the train images
+    """
     index = random.randint(0, 59999)
     (x_train, y_train), (x_test, y_test) = loadMNIST()
     print("Image number {index}. It is a {number}".format(index=index, number=y_train[index]))
@@ -29,6 +47,9 @@ def plotMNIST_random():
 
 
 def plotMNIST(index):
+    """
+        plots an image of the x_test images
+    """
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     plt.imshow(x_test[index], cmap="Greys")
     plt.show()
--- network.py
+++ network.py
@@ -4,32 +4,57 @@ import loadMNIST
 
 
 def sigmoid(z):
+    """
+    returns:
+            sigmoid result
+    """
     return 1.0/(1.0+np.exp(-z))
 
 
 def sigmoid_derivative(z):
+    """
+        returns:
+                derivative of sigmoid function
+    """
     return np.exp(z)/((np.exp(z)+1)**2)
 
 
 class Network:
 
     def __init__(self, layers):
+        # layers: list -> each element describes the number of neurons in the layer
         self.layers = layers
+        # number of layers in the network
         self.len_layers = len(layers)
+        # weights: list of matrices, biases: list of vectors
         self.weights, self.biases = self.initialize(self.layers)
 
     def initialize(self, layers):
-        #/np.sqrt(b)
+        """
+            returns:
+                    random weights: list, random biases: list
+        """
+        # /np.sqrt(b)
         weights = [np.random.randn(a, b) for a, b in zip(layers[1:], layers[0:])]
         biases = [np.random.randn(layer) for layer in layers[1:]]
         return weights, biases
 
     def feedforward(self, a):
+        """
+            parameter:
+                        a: activation_in -> vector
+            returns:
+                    activation_out
+        """
         for w, b in zip(self.weights, self.biases):
             a = sigmoid(np.dot(w, a) + b)
         return a
 
     def all_as_zs(self, a_in):
+        """
+            returns:
+                    list of all activations and all z's
+        """
         activation = [a_in]
         a = a_in
         Z = []
@@ -41,10 +66,18 @@ class Network:
         return activation, Z
 
     def predict(self, a_in):
+        """
+            predicts the label of the input image
+            returns:
+                    the index of the max output neuron
+        """
         a_out = self.feedforward(a_in)
         return np.argmax(a_out)
 
     def SGD(self, train_data, test_data, learning_rate=3.0, epochs=10, mini_size=30):
+        """
+            creates mini batches and passes them to updateWB
+        """
         for epoch in range(1, epochs+1):
             print("Epoch: {epoch}".format(epoch=epoch))
             random.shuffle(train_data)
@@ -54,6 +87,12 @@ class Network:
             print("Performance at epoch {epoch}: {perf}".format(epoch=epoch, perf=self.performance(test_data)))
 
     def updateWB(self, mini_batch, lrate):
+        """
+            parameters:
+                        mini_batch: list of 30 training samples
+                        lreate: learning rate
+            updates the weights and biases with one mini batch
+        """
         length = len(mini_batch)
         change_w = [np.zeros(w.shape) for w in self.weights]
         change_b = [np.zeros(b.shape) for b in self.biases]
@@ -65,6 +104,14 @@ class Network:
         self.biases = [b-(lrate/length)*bc for b, bc in zip(self.biases, change_b)]
 
     def backprop(self, y, a_in):
+        """
+            parameters:
+                        y: label vector
+                        a_in: input activation
+            executes backpropagation algorithm
+            returns:
+                    a list of biases and and a list of weights (change)
+        """
         change_w = []
         change_b = []
         # feedforward
@@ -84,26 +131,44 @@ class Network:
         return change_w[::-1], change_b[::-1]
 
     def out_layer_error_cross_entropy(self, a_out, y):
+        """
+            returns:
+                    cross_entropy output error
+        """
         return (a_out-y)
 
     def cost_function(self, a_out, y, z):
+        """
+            qudratic cost function
+        """
         return 0.5*(a_out-y)**2
 
     def cost_derivative(self, a_out, y, z):
+        """
+            derivative of quadratic cost function
+        """
         return (a_out-y)
 
-    def cross_entropy(self, a_out, y, z):
-        return (a_out-y)/sigmoid_derivative(z)
-
     def performance(self, test_data):
+        """
+            parameters:
+                        test_data: list of test data images
+            returns:
+                the sum of correct predicted images divided by the number of test_data samples
+        """
         return sum([net.predict(d) == np.argmax(l) for d, l in test_data])/len(test_data)
 
 
 if __name__ == "__main__":
+    # loads the mnist data
     (x_train, y_train), (x_test, y_test) = loadMNIST.loadMNIST()
     train_data = list(zip(x_train, y_train))
     test_data = list(zip(x_test, y_test))
+    # creates the network class: 3 layers
     net = Network([784, 30, 10])
+    # prints the performance before learning
     print(net.performance(test_data))
+    # network learns
     net.SGD(train_data, test_data)
+    # prints the performance after learning
     print("Resulted performance: {perf}".format(perf=net.performance(test_data)))
